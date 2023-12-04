fc8b4caa98fb775912c05c5aa8aa95897323e343
Linus Kardell <linus.kardell@gmail.com>
2023-12-04 09:26:36 +0100

    Add day 4

---
+++ day4/src/main.rs
@@ -0,0 +1,58 @@
+use std::io::Read;
+
+fn get_matches<'a>(line: &'a str, winning: &mut Vec<&'a str>) -> usize {
+    let mut words = line.split_whitespace().skip(2);
+    winning.clear();
+    while let Some(x) = words.next() {
+        if x == "|" {
+            break;
+        }
+
+        winning.push(x);
+    }
+
+    words.filter(|x| winning.contains(x)).count()
+
+}
+
+fn part_1(input: &str) -> u32 {
+    let mut winning = Vec::new();
+
+    input.lines().map(|line| {
+        let matches = get_matches(line, &mut winning);
+        if matches == 0 {
+            0
+        } else {
+            2_u32.pow((matches - 1) as u32)
+        }
+    }).sum()
+}
+
+fn part_2(input: &str) -> u32 {
+    let mut card_counts = vec![1u32];
+    let mut count = 0;
+    let mut winning = Vec::new();
+
+    for (n, line) in input.lines().enumerate() {
+        let end = n + get_matches(line, &mut winning) + 1;
+
+        if end > card_counts.len() {
+            card_counts.resize(end, 1);
+        }
+
+        for i in n + 1..end {
+            card_counts[i] += card_counts[n];
+        }
+
+        count += card_counts[n];
+    }
+
+    count
+}
+
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).unwrap();
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
+}
